900c1cf3850a4c2c06e1b0aa6845edd8e0165df1
Yaroslav Ryabukha <yaryabu@gmail.com>
2016-04-06 00:44:55 +0300

    Доделал аналитику и поправил несколько багов

---
+++ Source/VKDocReader/Classes/Presentation/Settings/NavBarOverlay.swift
@@ -0,0 +1,9 @@
+//
+//  NavBarOverlay.swift
+//  VKDocReader
+//
+//  Created by Yaroslav Ryabukha on 06/04/16.
+//  Copyright © 2016 Yaroslav Ryabukha. All rights reserved.
+//
+
+import Foundation
