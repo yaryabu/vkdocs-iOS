1aa3f9400ecbe67e526406ae32b64edc5ed518f9
Yaroslav Ryabukha <yaryabu@gmail.com>
2016-04-15 03:17:14 +0300

    Убрал говняшку

--- VKDocReader/Classes/Other/Localize.swift
+++ VKDocReader/Classes/Other/Localize.swift
@@ -12,7 +12,4 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: nil, bundle: NSBundle.mainBundle(), value: "", comment: "")
     }
-    
-    func efefe() {
-    }
 }
